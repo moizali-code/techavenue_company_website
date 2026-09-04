d4dc032e0769c8a506e4f5d5a96a531b8331c8ed
moiz ali <moiz.ali@techavenue.biz>
2026-09-04 16:52:14 +0500

    fixed map location

--- mock/contact.ts
+++ mock/contact.ts
@@ -29,7 +29,7 @@ export const contactSection: {
     businessHours: "Monday – Friday 9:00 AM – 6:00 PM",
     weekendHours: "Saturday / Sunday: Closed",
     mapEmbedUrl:
-      "https://www.google.com/maps?q=Evacuee+Trust+Complex,+F-5/1,+Islamabad&output=embed",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3318.056230091494!2d73.08594127418624!3d33.733362073278684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38dfc1ebcf0378e9%3A0xd55e427cc0603839!2sTech%20Avenue%20Pvt%20Ltd!5e0!3m2!1sen!2s!4v1788522597063!5m2!1sen!2s",
     directionsUrl:
       "https://www.google.com/maps/dir/?api=1&destination=Evacuee+Trust+Complex,+F-5/1,+Islamabad",
   },
